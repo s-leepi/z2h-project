b336ca0cea4ea0f01ac2e5ed2d8c20c05ac0be03
s-leepi <tigehr20@gmail.com>
2025-07-13 12:22:16 +0100

    first commit

---
+++ src/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]) {
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n	-  create new database file\n");
+	printf("\t -f	-  (required) path to database file\n");
+}
+
+int main(int argc, char *argv[]) { 
+	
+	char *filepath = NULL;
+	char *addstring = NULL;
+	bool newfile = false;
+	int c = 0;
+	int dbfd = -1;
+	struct dbheader_t *header = NULL;
+	struct employee_t *employees = NULL;
+
+	while((c = getopt(argc, argv, "nf:a:")) != -1) {
+		switch (c) {
+		
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case 'a':
+				addstring = optarg;
+				break;
+			case '?':
+				printf("Unknown option -%c\n", c);
+				break;
+
+			default: 
+				return -1;
+		}
+	}
+
+	if (filepath == NULL) {
+		printf("File path is a required argument\n");
+		print_usage(argv);
+		
+		return 0;
+	}
+
+	if (newfile) {
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to create database file\n");
+			return -1;
+		}
+
+		create_db_header(dbfd, &header);
+
+	} else {
+
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR) {
+			printf("Unable to open database file\n");
+			return -1;
+		}
+
+		if (validate_db_header(dbfd, &header) == STATUS_ERROR){
+			printf("Failed to validate database header\n");
+			return -1;
+		}
+	}
+
+	if (read_employees(dbfd, header, &employees) == STATUS_ERROR) {
+		printf("Failed to read employees\n");
+		return -1;
+	}
+
+	if (addstring) {
+		header->count++;
+		employees = realloc(employees, header->count*(sizeof(struct employee_t)));
+		add_employee(header, employees, addstring);
+	}
+
+	output_file(dbfd, header, employees);
+
+	close(dbfd);
+
+	return 0;
+}
